c10f41fa4277892b4ee6da555a5f5e20393750cf
Rafi Mohammed <mdrafi.f488@gmail.com>
2023-03-17 20:05:53 +0530

    code commited

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/cicd", (req, res) -> " This is CICD from Local");
+		get("/hello1", (req, res) -> " This is CICD from Local");
 	}
 }
